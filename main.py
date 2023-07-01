f382dc7a21c432564bcbe08e438506c5e5892408
ZeroNiki <nikitatop2222@gmail.com>
2023-07-01 12:04:32 +0500

    add inline keyboard and audio format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yt_dlp as ytd
 from aiogram import Dispatcher, Bot, executor, types
 from aiogram import asyncio
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.dispatcher.filters import Text
 from config import TOKEN
 
@@ -10,10 +11,47 @@ dp = Dispatcher(bot)
 # Начало
 @dp.message_handler(commands='start')
 async def hello(message: types.Message):
+    # Обычные кнопки
     start_button = ['Установить']
     keyboards = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboards.add(*start_button)
-    await message.answer("Привет! Это бот установщик видео с youtube", reply_markup=keyboards)
+
+    # Inline кнопки
+    link_buttons = [
+        InlineKeyboardButton(text='GitHub', url='https://github.com/ZeroNiki/Youtube-Downloader-Bot')
+    ]
+    link_keyboards = InlineKeyboardMarkup(row_width=1) 
+    link_keyboards.add(*link_buttons)
+
+    await message.answer("Привет! Это бот установщик видео с youtube", reply_markup=link_keyboards)
+    await message.answer("↑↑↑↑ Подпишитесь на мой github ↑↑↑↑", reply_markup=keyboards)
+
+#Inline кнопка устоновки в формате .mp4
+@dp.callback_query_handler(text='download_mp4')
+async def inline_keyboard_mp4(call: types.CallbackQuery):
+    options = {'skip-download': True, 'format': 'mp4'}
+
+    with ytd.YoutubeDL(options) as ytdl:
+        ytdl.download([link])
+        result = ytdl.extract_info("{}".format(link))
+        title = ytdl.prepare_filename(result)
+        video = open(f'{title}', 'rb')
+
+        await call.message.answer_video(video=video)
+
+#Inline кнопка устоновки в формате .mp4
+@dp.callback_query_handler(text='download_mp3')
+async def inline_keyboard_mp3(call: types.CallbackQuery):
+    options = {'skip-download': True, 'format': 'mp4'}
+
+    with ytd.YoutubeDL(options) as ytdl:
+        ytdl.download([link])
+        result = ytdl.extract_info("{}".format(link))
+        title = ytdl.prepare_filename(result)
+        audio = open(f'{title}', 'rb')
+
+        await call.message.answer_audio(audio=audio)
+
 
 # Установить
 @dp.message_handler(Text(equals='Установить'))
@@ -22,16 +60,16 @@ async def start_dw(message: types.Message):
 
 @dp.message_handler(content_types='text')
 async def downloading(message: types.Message):
+    global link
     link = message.text
+    keyboard = InlineKeyboardMarkup()
 
-    options = {'skip-download': True, 'format': 'mp4'}
+    keyboard.add(
+        InlineKeyboardButton(text='.mp4', callback_data='download_mp4'),
+        InlineKeyboardButton(text='.mp3', callback_data='download_mp3')
+        )
 
-    with ytd.YoutubeDL(options) as ytdl:
-        ytdl.download([link])
-        result = ytdl.extract_info("{}".format(link))
-        title = ytdl.prepare_filename(result)
-        video = open(f'{title}', 'rb')
-        await bot.send_video(message.chat.id, video=video)
+    await message.answer(message.text, reply_markup=keyboard)
 
 
 if __name__ == '__main__':
